a893214d5ac469dffe6327cd4573e85b303ef392
Zachary Friedman <zafriedman@gmail.com>
2014-08-18 12:01:08 -0700

    Add IkeaOneboxSpec

--- lib/onebox/engine/ikea_onebox.rb
+++ lib/onebox/engine/ikea_onebox.rb
@@ -18,7 +18,7 @@ module Onebox
           image: (og_raw.images.first if og_raw.images && og_raw.images.first),
           description: og_raw.description,
           type: (og_raw.type if og_raw.type),
-          price_cents: Monetize.parse(raw.xpath("/html/head").xpath('//meta[@name="price"]/@content').first.value).cents
+          price_cents: Monetize.parse(raw.xpath("/html/head").xpath('//meta[@name="price"]/@content').first.value).cents.to_s
         }
       end
     end
---
+++ spec/lib/onebox/engine/ikea_onebox_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe Onebox::Engine::IkeaOnebox do
+  let(:link) { "http://www.ikea.com/us/en/catalog/products/S89826677/" }
+  let(:json) { described_class.new(link).data }
+
+  before do
+    fake(link, response("ikea"))
+  end
+
+  it "should return a list of product attributes" do
+    expect(json).to eq(
+      {
+        link: "http://www.ikea.com/us/en/catalog/products/S89826677/",
+        title: "AKURUM Base cab for DOMSJÖ 2 bowl sink - white, Ädel medium brown, 36",
+        image: "http://www.ikea.com/us/en/images/products/akurum-base-cab-for-domsjo--bowl-sink__28343_PE114994_S4.JPG",
+        description: "IKEA - AKURUM, Base cab for DOMSJÖ 2 bowl sink, white, Ädel medium brown, 36",
+        type: "product",
+        price_cents: "19500"
+      }
+    )
+  end
+end
