fb4ecb0235f83e0be045b537522259cef022389e
Zachary Friedman <zafriedman@gmail.com>
2014-08-18 15:37:26 -0700

    Add overstock onebox

--- lib/onebox/engine/overstock_onebox.rb
+++ lib/onebox/engine/overstock_onebox.rb
@@ -1,10 +1,10 @@
 module Onebox
   module Engine
-    class AllmodernOnebox
+    class OverstockOnebox
       include Engine
       include HTMLEmbed
 
-      matches_regexp(/^http:\/\/(?:www)\.allmodern\.com\//)
+      matches_regexp(/^https:\/\/(?:www)\.overstock\.com\//)
 
       def data
         if og_raw.is_a?(Hash)
@@ -16,9 +16,9 @@ module Onebox
           link: link,
           title: og_raw.title,
           image: (og_raw.images.first if og_raw.images && og_raw.images.first),
-          description: og_raw.description,
+          description: raw.xpath("/html/head").xpath('//meta[@name="description"]/@content').first.value.gsub(/\u0099/, ""),
           type: (og_raw.type if og_raw.type),
-          price_cents: Monetize.parse(raw.xpath("/html/head").xpath('//meta[@property="og:price:amount"]/@content').first.value).cents.to_s
+          price_cents: Monetize.parse(raw.xpath('//span[@itemprop="price"]').children).cents.to_s
         }
       end
     end
