37c45867a19eeff7ec8d7fad8811b2a77e7386db
Jen <j.aprahamian@gmail.com>
2014-08-19 11:01:46 -0700

    Add spec and engine for World Market onebox

---
+++ lib/onebox/engine/world_market_onebox.rb
@@ -0,0 +1,28 @@
+module Onebox
+  module Engine
+    class WorldMarketOnebox
+      include Engine
+      include HTMLEmbed
+
+      matches_regexp(/^http:\/\/(?:www)\.worldmarket\.com\/product\//)
+
+      def data
+        if og_raw.is_a?(Hash)
+          og_raw[:link] ||= link
+          return og_raw
+        end
+
+        {
+          link: link,
+          title: og_raw.title,
+          image: (og_raw.images.first if og_raw.images && og_raw.images.first),
+          description: og_raw.description,
+          type: (og_raw.type if og_raw.type),
+          price_cents: Monetize.parse(raw.css('#productPricing > span')).cents.to_s
+        }
+      end
+    end
+  end
+end
+
+
