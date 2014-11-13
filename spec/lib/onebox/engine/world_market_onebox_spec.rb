bb8e65bb55f3322483c2f1db843aff6421a0b48d
Jen <j.aprahamian@gmail.com>
2014-11-12 18:24:07 -0800

    return nil for price when ranged (World Market)

--- spec/lib/onebox/engine/world_market_onebox_spec.rb
+++ spec/lib/onebox/engine/world_market_onebox_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Onebox::Engine::WorldMarketOnebox do
-  let(:link) { "http://www.worldmarket.com/product/bronze+perforated+hanging+pendant+lamp.do?&from=fn" }
+  let(:link) { "http://www.worldmarket.com/product/holiday+bird+coir+doormat.do?&from=fn" }
   let(:json) { described_class.new(link).data }
 
   before do
@@ -11,12 +11,12 @@ describe Onebox::Engine::WorldMarketOnebox do
   it "should return a list of product attributes" do
     expect(json).to eq(
       {
-        link: "http://www.worldmarket.com/product/bronze+perforated+hanging+pendant+lamp.do?&from=fn",
-        title: "Bronze Perforated Hanging Pendant Lamp",
-        image: "http://ii.worldmarket.com/fbog/500/40961_XXX_v1.jpg?Bronze-Perforated-Hanging-Pendant-Lamp",
-        description: "Pierced with precision pinholes in varying sizes, our exclusive Bronze Perforated Hanging Pendant Lamp fills your space with irregular rays of light. Crafted in India of iron with rounded contours finished in bronze, this unique pendant adds romantic ambience anywhere it hangs.",
+        link: "http://www.worldmarket.com/product/holiday+bird+coir+doormat.do?&from=fn",
+        title: "Holiday Bird Coir Doormat",
+        image: "http://ii.worldmarket.com/fbog/500/43435_XXX_v1.jpg?Holiday-Bird-Coir-Doormat",
+        description: "Crafted of coconut husk fibers and printed with our exclusive holiday design, our Holiday Bird Coir Doormat adds a perfect seasonal welcome at a great price.",
         type: "product",
-        price_cents: "7999"
+        price_cents: "999"
       }
     )
   end
