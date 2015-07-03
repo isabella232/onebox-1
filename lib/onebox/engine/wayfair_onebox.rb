b48721441aa56f3b4ad6bb0c834feb5a05bf8ed2
Jen <j.aprahamian@gmail.com>
2015-07-02 17:24:21 -0700

    handle wayfair items without OG tags, price and type will be nil for these bc likely daily sale

--- lib/onebox/engine/wayfair_onebox.rb
+++ lib/onebox/engine/wayfair_onebox.rb
@@ -6,6 +6,47 @@ module Onebox
 
       matches_regexp(/^http:\/\/(?:www)\.wayfair\.com\//)
 
+      def title
+        if og_raw.title
+          # og_raw.title
+          "no"
+        else
+          raw.css('#bd > div.prodnameshare > h1').inner_html.gsub(/<[^>]+>/, '').gsub(/\n/, '').gsub(/\s{2,}/, '')
+        end
+      end
+
+      def price
+        if raw.xpath("/html/head").xpath('//meta[@property="og:price:amount"]/@content').empty?
+          nil
+        else
+          Monetize.parse(raw.xpath("/html/head").xpath('//meta[@property="og:price:amount"]/@content')).cents.to_s
+        end
+      end
+
+      def image
+        if og_raw.images && og_raw.images.first
+          og_raw.images.first
+        else
+          product_main_img = raw.css('img.product_main_img').first['src']
+        end
+      end
+
+      def description
+        if og_raw.description
+          og_raw.description
+        else
+          raw.xpath('/html/head').xpath('//meta[@name="description"]/@content').first.value.gsub(/<[^>]+>/, '')
+        end
+      end
+
+      def type
+        if og_raw.type
+          og_raw.type
+        else
+          nil
+        end
+      end
+
       def data
         if og_raw.is_a?(Hash)
           og_raw[:link] ||= link
@@ -14,11 +55,11 @@ module Onebox
 
         {
           link: link,
-          title: og_raw.title,
-          image: (og_raw.images.first if og_raw.images && og_raw.images.first),
-          description: og_raw.description,
-          type: (og_raw.type if og_raw.type),
-          price_cents: Monetize.parse(raw.xpath("/html/head").xpath('//meta[@property="og:price:amount"]/@content').first.value).cents.to_s
+          title: title,
+          image: image,
+          description: description,
+          type: type,
+          price_cents: price
         }
       end
     end
