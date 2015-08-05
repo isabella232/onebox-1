7d6b394353e0cdf66232af2c7619a27eb35b24b8
Michael Graham <michael@endlessdoloop.com>
2015-08-05 11:50:24 -0700

    Restoration Hardware engine

---
+++ lib/onebox/engine/restoration_hardware_onebox.rb
@@ -0,0 +1,50 @@
+module Onebox
+  module Engine
+    class RestorationHardwareOnebox
+      include Engine
+      include HTMLEmbed
+
+      matches_regexp(/^http:\/\/(?:www)\.restorationhardware\.com\//)
+
+      def title
+        og_raw.title
+      end
+
+      def price
+        amount = raw.css('[itemprop=price]')[0].text
+        amount.empty? ? nil : Onebox::Helpers.squish(amount)
+      end
+
+      def price_cents
+        Monetize.parse(price).cents.to_s
+      end
+
+      def image
+        image = raw.css('#default-photo')[0]
+        return unless image
+        src = image['src']
+        "http:#{src}"
+      end
+
+      def description
+        og_raw.description
+      end
+
+      def type
+        og_raw.type
+      end
+
+      def data
+        {
+          link: link,
+          title: title,
+          image: image,
+          description: description,
+          type: type,
+          price: price,
+          price_cents: price_cents
+        }
+      end
+    end
+  end
+end
